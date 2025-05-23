a7ff1378a518254b8d7d8f9fd45f0ce0787f36e2
PAOLA SINAY AGUILAR MARTÍNEZ <psinay.aguilar@edu.uag.mx>
2025-05-22 21:27:17 -0600

    1.7: unicafe, paso 2 Amplía tu aplicación para que muestre más estadísticas sobre los comentarios recopilados: el número total de comentarios recopilados, la puntuación promedio (buena: 1, neutral: 0, mala: -1) y el porcentaje de comentarios positivos.

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -10,6 +10,9 @@ const Button = ({ handleClick, text }) => (
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
+  const average = all ? (good - bad) / all : 0
+  const positive = all ? (good / all) * 100 : 0
+
 
   return (
     <div>
@@ -18,6 +21,9 @@ const Statistics = ({ good, neutral, bad }) => {
           <tr><td>good</td><td>{good}</td></tr>
           <tr><td>neutral</td><td>{neutral}</td></tr>
           <tr><td>bad</td><td>{bad}</td></tr>
+          <tr><td>all</td><td>{all}</td></tr>
+          <tr><td>average</td><td>{average}</td></tr>
+          <tr><td>positive</td><td>{positive} %</td></tr>
         </tbody>
       </table>
     </div>
@@ -52,7 +58,6 @@ const App = () => {
       <Button handleClick={handleBadClick} text='Bad' />
       <Text text={subtitle} />
       <Statistics good={good} neutral={neutral} bad={bad} />
-
     </div>
   )
 }
