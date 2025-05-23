eeb37b65345054e2eaaed9842f2b61499389e799
PAOLA SINAY AGUILAR MARTÍNEZ <psinay.aguilar@edu.uag.mx>
2025-05-22 21:19:14 -0600

    1.6: unicafe, paso 1 Como la mayoría de las empresas, Unicafe recopila comentarios de sus clientes. Tu tarea es implementar una aplicación web para recopilar comentarios de los clientes. Solo hay tres opciones para los comentarios: good (bueno), neutral y bad(malo).

---
+++ unicafe/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+const Text = ({ text }) => <h1>{text}</h1>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr><td>good</td><td>{good}</td></tr>
+          <tr><td>neutral</td><td>{neutral}</td></tr>
+          <tr><td>bad</td><td>{bad}</td></tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const title = 'give feedback'
+  const subtitle = 'statistics'
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+  setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Text text={title} />
+      <Button handleClick={handleGoodClick} text='Good' />
+      <Button handleClick={handleNeutralClick} text='Neutral' />
+      <Button handleClick={handleBadClick} text='Bad' />
+      <Text text={subtitle} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
+    </div>
+  )
+}
+
+export default App
