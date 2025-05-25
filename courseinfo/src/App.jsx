b7e52575c1fac5e5bff5558a4b4113d568e66105
PAOLA SINAY AGUILAR MARTÍNEZ <psinay.aguilar@edu.uag.mx>
2025-05-25 13:37:55 -0600

    2.5: Módulo separado paso 10 Declara el componente Course como un módulo separado, que se importa en el componente App. Puedes incluir todos los subcomponentes del curso en el mismo módulo.

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,35 +1,5 @@
-const Header = ({ course }) => {
-  return <h2>{course}</h2>  
-}
-
-const Part = ({ name, exercises }) => {
-  return <p>{name} {exercises}</p>
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <p><strong>total of {total} exercises</strong></p>
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import React from 'react'
+import Course from './Course' // Importing the Course component
 
 const App = () => {
   const courses = [
