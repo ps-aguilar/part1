d02310e42633e3f15931b0ee63704b7a1e78b477
PAOLA SINAY AGUILAR MARTÍNEZ <psinay.aguilar@edu.uag.mx>
2025-05-25 14:27:32 -0600

    2.6: La Agenda Telefónica Paso 1 Creemos una agenda telefónica sencilla. En esta parte solo agregaremos nombres a la agenda.

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
