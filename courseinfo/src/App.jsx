ee9e45cf11c96da9388869d1a7f667dbc82e66a3
PAOLA SINAY AGUILAR MARTÍNEZ <psinay.aguilar@edu.uag.mx>
2025-05-25 12:12:24 -0600

    2.1: Información del curso paso 6
    Define un componente responsable de formatear un solo curso llamado Course.

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,58 +1,54 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
 }
 
-const Part = (props) => {
-  return (
-    <p>{props.name} {props.exercises}</p>
-  )
+const Part = ({ name, exercises }) => {
+  return <p>{name} {exercises}</p>
 }
 
-const Content = (props) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
+      {parts.map(part => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
     </div>
   )
 }
 
-const Total = (props) => {
+const Course = ({ course }) => {
   return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
   )
 }
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-
-  return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
