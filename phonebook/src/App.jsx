fb1c88e39bc39ffaec0165114c9cd1c14431568e
PAOLA SINAY AGUILAR MARTÍNEZ <psinay.aguilar@edu.uag.mx>
2025-05-25 14:42:39 -0600

    2.7: La Agenda Telefónica Paso 2 Evita que el usuario pueda agregar nombres que ya existen en la agenda telefónica. Los arrays de JavaScript tienen numerosos métodos adecuados para realizar esta tarea.

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -12,6 +12,14 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
+    
+    const nameExists = persons.some(person => person.name === newName)
+    
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
     const newPerson = { name: newName }
     setPersons(persons.concat(newPerson))
     setNewName('')
